f8ff43197b91454c5843577953c57ad2c9ca2436
joshna7 <mandalajoshna@gmail.com>
2021-08-18 16:04:44 +0000

    3rd commit

--- demo1.java
+++ demo1.java
@@ -1,8 +1,12 @@
 class demo1
 {
 	public static void main(String args[]){
-	system.out.println("Welcome to git")
+	
+		system.out.println("Welcome to git")
+                system.out.println("Welcome to git2")
+
 	}
+
 
 }
 
